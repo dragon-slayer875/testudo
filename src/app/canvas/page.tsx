276096bcbe8830636ab4c0c6158806f0b460509e
Rudraksh Tyagi <rudraksh875@gmail.com>
2024-03-01 20:16:17 +0530

    remove manual implementaiton of zoom and panning

--- src/app/canvas/page.tsx
+++ src/app/canvas/page.tsx
@@ -3,7 +3,7 @@ import Canvas from "@/components/ui/Canvas/Canvas";
 
 export default function Page(): JSX.Element {
     return (
-        <main className="w-full min-h-[100dvh]">
+        <main className="w-full h-screen">
             <Canvas />
         </main> 
     );
