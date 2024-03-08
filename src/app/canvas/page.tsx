6c1c968c17ae290c6d1935c9a2316562e7c91912
Rudraksh Tyagi <rudraksh875@gmail.com>
2024-03-09 01:45:32 +0530

    add Provider wrapper in canvas page

--- src/app/canvas/page.tsx
+++ src/app/canvas/page.tsx
@@ -1,11 +1,16 @@
+"use client";
 import Commander from "@/components/ui/Commander";
 import Canvas from "@/components/ui/Canvas/Canvas";
+import CanvasStore from "@/stateManagement/store";
+import { Provider } from "react-redux";
 
 export default function Page(): JSX.Element {
     return (
-        <main className="overflow-hidden">
-            <Commander />
-            <Canvas />
-        </main> 
+        <Provider store={CanvasStore}>
+            <main className="overflow-hidden">
+                <Commander />
+                <Canvas />
+            </main>
+        </Provider>
     );
 }
